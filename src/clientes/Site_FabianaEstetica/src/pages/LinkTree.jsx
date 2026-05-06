62e81f1e3f03c4b9004c899ec664094b14f03696
GAMIN6031 <pedrocorreajogo@gmail.com>
2026-05-06 10:56:44 -0300

    Removendo todos os arquivos

--- src/clientes/Site_FabianaEstetica/src/pages/LinkTree.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import LinkTreeSection from "../components/sections/Linktree/LinkTree";
-
-const LinkTreePage = () => {
-  return <LinkTreeSection />;
-};
-
-export default LinkTreePage;
