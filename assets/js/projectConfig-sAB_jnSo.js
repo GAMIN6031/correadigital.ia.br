6f5d62caa24fb52aa7f2401ca95f3474481e4659
GAMIN6031 <pedrocorreajogo@gmail.com>
2026-06-01 16:53:48 -0300

    Updates

---
+++ assets/js/projectConfig-sAB_jnSo.js
@@ -0,0 +1,1 @@
+var a={id:"loja",rota:"/"};export{a as default};
---
+++ assets/js/projectTemplate-DR0T_yfZ.js
@@ -0,0 +1,1 @@
+var e={id:"seu-projeto",name:"Nome do seu projeto",route:"/seu-projeto",theme:"seu-projeto",pages:[{id:"home",path:"Home",name:"Página Inicial"}]};export{e as projectTemplate};
